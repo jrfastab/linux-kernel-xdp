52308b4c21c0e53800c9a0ce2d23542d987c2461
John Fastabend <john.fastabend@gmail.com>
2017-06-16 16:30:47 -0700

    xdp: redirect, allow driver to push ctx pointer with redirect
    The current XDP flow is for a driver to post a set of packets using
    the do_xdp_redirect() hook and then to flush the packets later
    with a do_xdp_flush() call. This allows redirect to be used with
    batching delaying the tail write until the entire set of packets
    have been posted.
    
    However, on the flush the driver posting the redirect must "know"
    which TX queue to flush. This patch enhances the API so that the
    driver can post a context pointer to be returned with the flush
    API. This should allow the driver to flush only the required TX
    queue. Note the context pointer if a (void *) type and is not read
    or used by the core logic.
    
    Additionally, now that we return a pointer from the redirect ndo op
    return a NULL pointer when an error occurs before the pkt is enqueued
    in the tx ring. This allows the caller to recycle the buffer in
    directly in the fast path rather than use page_frag_free() or other
    methods.
    
    Signed-off-by: John Fastabend <john.fastabend@gmail.com>

--- include/linux/bpf.h
+++ include/linux/bpf.h
@@ -375,7 +375,18 @@ void bpf_user_rnd_init_once(void);
 u64 bpf_user_rnd_u32(u64 r1, u64 r2, u64 r3, u64 r4, u64 r5);
 
 /* Map specifics */
-struct net_device  *__dev_map_lookup_elem(struct bpf_map *map, u32 key);
+struct bpf_dtab_netdev {
+	struct net_device *dev;
+	void __percpu **ctx;
+	int key;
+	struct rcu_head rcu;
+	struct bpf_dtab *dtab;
+};
+
+struct bpf_dtab_netdev  *__dev_map_lookup_elem(struct bpf_map *map, u32 key);
+void __dev_map_insert_ctx(struct bpf_map *map,
+			  struct bpf_dtab_netdev *netdev,
+			  void *ctx, u32 index);
 void __dev_map_flush(struct bpf_map *map);
 
 #endif /* _LINUX_BPF_H */
