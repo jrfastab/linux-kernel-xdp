f2c0c493434a7bf5abd765ea7e8528278fb4c8fa
Tom Herbert <tom@herbertland.com>
2017-07-25 07:41:16 -0700

    Implement an in-kernel proxy.
    This patch defines a new address family for create a kproxy socket
    (AF_KPROXY). Two IOCTL operations are defined SIOCKPROXYJOIN and
    SIOCKPROXYUNJOIN.
    
    SIOCKPROXYJOIN takes two (TCP) sockets and creates a proxy between
    them. An optional stream parser BPF program is taken for each socket. If
    none is provided then, although the stream parser is still run, the
    default parser is used which returns skb->len. To support multiple
    TCP endpoints in the proxy the user may run multiple SIOCKPROXYJOIN
    commands and provide an optional BPF program (bpf_fd_mux) program.
    For this to work the user must also specify a max_peers which the
    control path and datapath use to build data structures to hold the
    multiple endpoints. The ideal user space application should set this
    to the smalled value possible. When multiple endpoints are being
    used userspace gives each endpoin an ID called {client,server}_index.
    These are the return codes used by the BPF mux program to specify
    an endpoint. Returning a negative value or value that does not map
    to an active ID causes the packet to be dropped.
    
    (code note: document the return codes and tighten up spec here)
    
    (code note: should we just 'grow' this in the code automatically and
    remove this option, further we can change the list walks to something
    better, maybe BST, if needed but at the moment my multiple endpoint
    count is small and I think this is an optimization)
    
    SIOCKPROXYUNJOIN is used to unjoin two TCP sockets. In the multiple
    endpoint case when reference count reaches zero the socket is removed
    from the kproxy. In the 1:1 case this means refcnt goes to zero and
    the kproxy no longer has any sockets to manage.
    
    A proc file (/prox/net/kproxy) is created to list all the running
    kernel proxies and relevant statistics for them.
    
    (code note: converting to diag_sock currently)
    
    A new sample program is added in ./samples/kproxy to build a kproxy
    object with two backend endpoints and a single sending frontend.
    
    (code note: OK the sample program is still clumsy but it is good
    for testing for now.)
    
    (code note: I dropped the TX worker thread and created a dead lock
    where user joins recv and sendmsg side of the same socket. We can
    either catch this in the ioctl handler or add the worker thread
    back. For now it deadlocks.)
    
    Signed-off-by: Tom Herbert <tom@herbertland.com>
    Signed-off-by: John Fastabend <john.fastabend@gmail.com>

--- include/linux/socket.h
+++ include/linux/socket.h
@@ -206,8 +206,9 @@ struct ucred {
 				 * PF_SMC protocol family that
 				 * reuses AF_INET address family
 				 */
+#define AF_KPROXY	44	/* Kernel Proxy */
 
-#define AF_MAX		44	/* For now.. */
+#define AF_MAX		45	/* For now.. */
 
 /* Protocol families, same as address families. */
 #define PF_UNSPEC	AF_UNSPEC
@@ -257,6 +258,7 @@ struct ucred {
 #define PF_QIPCRTR	AF_QIPCRTR
 #define PF_SMC		AF_SMC
 #define PF_MAX		AF_MAX
+#define PF_KPROXY	AF_KPROXY
 
 /* Maximum queue length specifiable by listen.  */
 #define SOMAXCONN	128
