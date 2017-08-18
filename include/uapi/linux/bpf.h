17e0965b2bc3fc5d66f95f307bdfe4cb9f11def4
John Fastabend <john.fastabend@gmail.com>
2017-08-18 09:04:52 -0700

    bpf: sockmap, remove STRPARSER map_flags and add multi-map support
    The addition of map_flags BPF_SOCKMAP_STRPARSER flags was to handle a
    specific use case where we want to have BPF parse program disabled on
    an entry in a sockmap.
    
    However, Alexei found the API a bit cumbersome. Lets remove it and
    support the use case by allowing socks to be in multiple maps. This
    allows users to create two maps one with programs attached and one
    without. When socks are added to maps they now inherit any programs
    attached to the map. This is a nice generalization and IMO improves
    the API.
    
    The API rules are less ambiguous and do not need a flag:
    
      - When a sock is added to a sockmap we have two cases,
    
         i. The sock map does not have any attached programs so
            we can add sock to map without inheriting bpf programs.
            The sock may exist in 0 or more other maps.
    
        ii. The sock map has attached BPF program. To avoid duplicate
            bpf programs we only add the sock entry if it does not have
            an existing strparser attached (return -EBUSY here). Otherwise
            attach the program and inherit strparser/verdict programs from
            the sock map.
    
    This allows for socks to be in a multiple maps for redirects and
    inherit a BPF program from a single map.
    
    Samples and selftests are also updated to continue running. Self
    tests was updated to cover the single sock in multi-map case.
    
    Note: Now that multiple maps are supported the "maps" pointer called
    when a socket is closed becomes a list of maps to remove the sock from.
    
    Suggested-by: Alexei Starovoitov <ast@kernel.org>
    Signed-off-by: John Fastabend <john.fastabend@gmail.com>

--- include/uapi/linux/bpf.h
+++ include/uapi/linux/bpf.h
@@ -143,9 +143,6 @@ enum bpf_attach_type {
 
 #define MAX_BPF_ATTACH_TYPE __MAX_BPF_ATTACH_TYPE
 
-/* If BPF_SOCKMAP_STRPARSER is used sockmap will use strparser on receive */
-#define BPF_SOCKMAP_STRPARSER	(1U << 0)
-
 /* If BPF_F_ALLOW_OVERRIDE flag is used in BPF_PROG_ATTACH command
  * to the given target_fd cgroup the descendent cgroup will be able to
  * override effective bpf program that was inherited from this cgroup
@@ -573,14 +570,11 @@ union bpf_attr {
  *     @flags: reserved for future use
  *     Return: SK_REDIRECT
  *
- * int bpf_sock_map_update(skops, map, key, flags, map_flags)
+ * int bpf_sock_map_update(skops, map, key, flags)
  *	@skops: pointer to bpf_sock_ops
  *	@map: pointer to sockmap to update
  *	@key: key to insert/update sock in map
  *	@flags: same flags as map update elem
- *	@map_flags: sock map specific flags
- *	   bit 1: Enable strparser
- *	   other bits: reserved
  */
 #define __BPF_FUNC_MAPPER(FN)		\
 	FN(unspec),			\
